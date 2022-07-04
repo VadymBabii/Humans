39cfea5600b7478e3909b533dd8df1408e15268d
vadym.babii <vadym.babii@litslink.com>
2022-07-04 12:04:43 +0300

    sas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header/Header";
-import "semantic-ui-css/semantic.min.css";
+// import "semantic-ui-css/semantic.min.css";
 import Notes from "./pages/Notes/Notes";
 
 function App() {
